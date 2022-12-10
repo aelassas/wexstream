c5618e5c6465b05b43171636ca51002074213d76
aelassas <akram.elassas@gmail.com>
2022-12-10 12:46:18 +0100

    Update Contact.js

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,68 +1,20 @@
-import React, { Component } from 'react';
-import Header from './Header';
+import React from 'react';
 import { strings } from '../config/app.config';
-import { LANGUAGES } from '../config/env.config';
-import { validateAccessToken, getCurrentUser, getUser, getLanguage, getQueryLanguage } from '../services/UserService';
+import Master from '../elements/Master.js';
 
 
-class Contact extends Component {
+const Contact = () => (
+    <Master>
+        <div className="content">
+            <div className="contact">
+                <h1>{strings.TECHNICAL_ISSUES_TITLE}</h1>
+                <p>{strings.TECHNICAL_ISSUES}</p>
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null
-        }
-    }
-
-    componentDidMount() {
-        let language = getQueryLanguage();
-
-        if (!LANGUAGES.includes(language)) {
-            language = getLanguage();
-        }
-        strings.setLanguage(language);
-        this.setState({});
-
-        const currentUser = getCurrentUser();
-        if (currentUser) {
-            validateAccessToken().then(status => {
-                if (status === 200) {
-                    getUser(currentUser.id).then(user => {
-                        if (user) {
-                            this.setState({ user });
-                        } else {
-                            this.setState({ user: null });
-                        }
-                    }).catch(err => {
-                        this.setState({ user: null });
-                    });
-                }
-            }).catch(err => {
-                this.setState({ user: null });
-            });
-        } else {
-            this.setState({ user: null });
-        }
-    }
-
-    render() {
-        const { user } = this.state;
-        return (
-            <div>
-                <Header user={user} />
-                <div className="content">
-                    <div className="contact">
-                        <h1>{strings.TECHNICAL_ISSUES_TITLE}</h1>
-                        <p>{strings.TECHNICAL_ISSUES}</p>
-
-                        <h1>{strings.OTHER_REQUESTS_TITLE}</h1>
-                        <p>{strings.OTHER_REQUESTS}</p>
-
-                    </div>
-                </div>
+                <h1>{strings.OTHER_REQUESTS_TITLE}</h1>
+                <p>{strings.OTHER_REQUESTS}</p>
             </div>
-        );
-    }
-}
+        </div>
+    </Master>
+);
 
 export default Contact;
