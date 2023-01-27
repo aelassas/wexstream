75f2ba93b305a5a8c2504250cd4c137f80277ea1
aelassas <akram.elassas@gmail.com>
2023-01-27 21:25:40 +0100

    Update ws-deploy.sh

--- __scripts/ws-deploy.sh
+++ __scripts/ws-deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "all" ]; then
-  /bin/bash /opt/wexstream/scripts/ws-deploy-api.sh
-  /bin/bash /opt/wexstream/scripts/ws-deploy-app.sh
+  /bin/bash /opt/wexstream/__scripts/ws-deploy-api.sh
+  /bin/bash /opt/wexstream/__scripts/ws-deploy-app.sh
 elif [ "$1" == "api" ]; then
-  /bin/bash /opt/wexstream/scripts/ws-deploy-api.sh
+  /bin/bash /opt/wexstream/__scripts/ws-deploy-api.sh
 elif [ "$1" == "app" ]; then
-  /bin/bash /opt/wexstream/scripts/ws-deploy-app.sh
+  /bin/bash /opt/wexstream/__scripts/ws-deploy-app.sh
 else
   echo "Usage: ws-deploy all|api|app"
 fi
