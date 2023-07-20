97f203bddd37d0a1acc3b3c70d524087a243953e
aelassas <akram.elassas@gmail.com>
2023-07-20 11:29:06 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,16 +1,16 @@
 import app from './server.js'
-import fs from 'fs'
+import fs from 'fs/promises'
 import https from 'https'
 
-const PORT = parseInt(process.env.WS_PORT) || 4000
+const PORT = parseInt(process.env.WS_PORT) || 4003
 const HTTPS = process.env.WS_HTTPS.toLocaleLowerCase() === 'true'
 const PRIVATE_KEY = process.env.WS_PRIVATE_KEY
 const CERTIFICATE = process.env.WS_CERTIFICATE
 
 if (HTTPS) {
     https.globalAgent.maxSockets = Infinity
-    const privateKey = fs.readFileSync(PRIVATE_KEY, 'utf8')
-    const certificate = fs.readFileSync(CERTIFICATE, 'utf8')
+    const privateKey = await fs.readFile(PRIVATE_KEY, 'utf8')
+    const certificate = await fs.readFile(CERTIFICATE, 'utf8')
     const credentials = { key: privateKey, cert: certificate }
     const httpsServer = https.createServer(credentials, app)
 
